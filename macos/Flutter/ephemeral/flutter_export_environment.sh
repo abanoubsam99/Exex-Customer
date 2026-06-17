26eda26dba615e67129438fb58fd6de58673dbe5
abanoubsam99 <abanoubsam1999@gmail.com>
2026-06-17 11:13:12 +0300

    ios

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\develop\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\MyWork\FreeLanceProject\EVEX\Evex-user-main"
+export "FLUTTER_ROOT=/Users/abanoubmac/develop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/MacHardSSD/MyProjects/Exex-Customer"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.2"
